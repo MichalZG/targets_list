e3a66e4d8618a18653b57e2217e7e010d8fdc1f5
michalzg <michalzejmo@gmail.com>
2020-08-03 01:16:50 +0200

    create superuser

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+source .env
 docker-compose run dbapp python manage.py migrate
 docker-compose run dbapp python manage.py makemigrations targets
 docker-compose run dbapp python manage.py migrate targets
-docker-compose run dbapp sh ./create_superuser.sh
+docker-compose run -e DATABASE_ADMIN_NAME=$DATABASE_ADMIN_NAME -e DATABASE_ADMIN_PASS=$DATABASE_ADMIN_PASS dbapp sh ./create_superuser.sh
