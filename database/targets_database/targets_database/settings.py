66aff3197e71018d7311d645c4d5462d38bcb507
michal <michalzejmo@gmail.com>
2020-08-02 13:21:30 +0200

    static files qfix

--- database/targets_database/targets_database/settings.py
+++ database/targets_database/targets_database/settings.py
@@ -39,9 +39,15 @@ if os.environ.get('DATABASE_DEBUG', 'False') == 'True':
 
 ALLOWED_HOSTS = ['*',]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static') 
+STATIC_URL = '/static/'
 
+"""
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+"""
 # Application definition
 
 INSTALLED_APPS = [
@@ -133,4 +139,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
